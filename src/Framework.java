c279785fb2a45c6f93ddb268aa91fe13a7253dd3
Tim Rietdijk <tim.is@live.nl>
2018-04-03 13:47:59 +0200

    framework update 2

--- src/Framework.java
+++ src/Framework.java
@@ -1,37 +1,23 @@
 public class Framework {
 
-    int[][] field;
-
-<<<<<<< HEAD
+    public int[][] field;
 
     
     private void generateField(int length, int width){
         field = new int[length][width];
         
-        for(int i=0; i<mat.length; i++) {
-            for(int j=0; j<mat[i].length; j++) {
+        for(int i=0; i<field.length; i++) {
+            for(int j=0; j<field[i].length; j++) {
                 System.out.println("Values at arr["+i+"]["+j+"] is "+mat[i][j]);
             }
-=======
-    public void generateField(int length, int width){
-        field = new int[length][width];
-
-        for(int i = 0; i < length; i++){
-            for(int j =0; j < width; i++){
-                System.out.println(i + " : " + j);
-          }
->>>>>>> a46b65aa688cc1265802cd7cdb8ffd2abc876763
         }
     }
 }
 
-<<<<<<< HEAD
-
-=======
-class main{
+public class main{
     public static void main(String[] args){
         Framework framework = new Framework();
         framework.generateField(12,12);
     }
 }
->>>>>>> a46b65aa688cc1265802cd7cdb8ffd2abc876763
+
