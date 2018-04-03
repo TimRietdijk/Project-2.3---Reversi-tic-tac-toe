2ab09c5bf47b33561e77bd8714f3012ac99e7f63
Tim Rietdijk <tim.is@live.nl>
2018-04-03 14:11:13 +0200

    basic field done!

--- src/Framework.java
+++ src/Framework.java
@@ -5,13 +5,20 @@ public class Framework {
     
     public void generateField(int length, int width){
         field = new int[length][width];
-        
-// test code
-//        for(int i=0; i<field.length; i++) {
-//            for(int j=0; j<field[i].length; j++) {
-//                System.out.println("Values at arr["+i+"]["+j+"] is "+field[i][j]);
-//            }
-//        }
+
+    }
+    public void getState(int length, int width) {
+    	System.out.println(field[length][width]);
+    }
+    public void setSate(int length, int width, int value) {
+    	field[length][width] = value;
+    }
+    public void showField() {
+	      for(int i=0; i<field.length; i++) {
+	      for(int j=0; j<field[i].length; j++) {
+	          System.out.println("Values at arr["+i+"]["+j+"] is "+field[i][j]);
+	      }
+	  }
     }
 }
 
@@ -19,6 +26,10 @@ class main{
     public static void main(String[] args){
         Framework framework = new Framework();
         framework.generateField(12,12);
+        
+        framework.setSate(5, 3, 1);
+        framework.showField();
+        
     }
 }
 
