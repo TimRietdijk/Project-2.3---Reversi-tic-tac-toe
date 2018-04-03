5645434bc5c2130d0d7275714d90b36a641997e1
Gerco Lubberink <gerco100@gmail.com>
2018-04-03 13:16:04 +0200

    Framework made

--- src/Framework.java
+++ src/Framework.java
@@ -1,3 +1,12 @@
 public class Framework {
 
+    int[][] field;
+
+    private void generateField(int length, int width){
+        field = new int[length][width];
+
+        for(){
+
+        }
+    }
 }
