8d34bcb76147fc590a475b1db6640dfa8edf53da
Tim Rietdijk <tim.is@live.nl>
2018-04-05 11:13:19 +0200

    cool superclass doet het

--- src/CommandCenter.java
+++ src/CommandCenter.java
@@ -1,3 +1,8 @@
+/**
+ * Deze class zet de verbinding op met de server
+ * en regelt alle mogelijke inkomende en uitgaande commando's
+ */
+
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
@@ -5,23 +10,29 @@ import java.util.Scanner;
 
 public class CommandCenter {
 
+    static Socket s;
+
     public CommandCenter() throws IOException {
-        // sendingCommand houdt het command dat naar de server verstuurd wordt
-        String sendingCommand;
-        Socket s = new Socket("127.0.0.1", 80);
+
+        setupConnection("127.0.0.1", 80);
         Scanner sc1 = new Scanner(s.getInputStream());
-        System.out.println("Send command to server");
-
-
         ReadReceived(sc1);
-        while(true) {
-            // sendingCommand afhankelijk van gedrukte knop door gebruiker
-            sendingCommand = "login <speler>"; // Bijvoorbeeld login commando
-            PrintStream p = new PrintStream(s.getOutputStream());
-            p.println(sendingCommand);
-        }
     }
 
+    // Connectie opzetten met server
+    public void setupConnection(String host, int port) throws IOException {
+        Socket s = new Socket(host, port);
+    }
+
+    // Versturen van commandos naar server
+    private static void sendCommand(String command) throws IOException {
+        // sendingCommand houdt het command dat naar de server verstuurd wordt
+        String sendingCommand = command; // Bijvoorbeeld login commando: "login <speler>"
+        PrintStream p = new PrintStream(s.getOutputStream());
+        p.println(sendingCommand);
+    }
+
+    // Ontvangst commandos van server
     private static void ReadReceived(Scanner sc1) {
         new Thread(new Runnable() {
             public void run(){
