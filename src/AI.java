efacbe730ae16c0ebc37a28c19f1fa5b19a49680
Winter Halbesma <whalbesma@gmail.com>
2018-04-09 12:02:43 +0200

    AI class added

---
+++ src/AI.java
@@ -0,0 +1,21 @@
+public class AI {
+    public class Board {
+
+    }
+
+    public interface Tile {
+
+    }
+
+    public class Empty implements Tile {
+
+    }
+
+    public class Ours implements Tile {
+
+    }
+
+    public class Traitors implements Tile {
+
+    }
+}
