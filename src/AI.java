029e0ac48c539cd46a7f345dc4cbfe3ed3673229
Winter Halbesma <whalbesma@gmail.com>
2018-04-09 12:41:59 +0200

    AI class update 2

--- src/AI.java
+++ src/AI.java
@@ -1,13 +1,29 @@
 public class AI {
     public class Board {
 
-        private Tile myTile;
+        private Tile myTile0;
+        private Tile myTile1;
+        private Tile myTile2;
+        private Tile myTile3;
+        private Tile myTile4;
+        private Tile myTile5;
+        private Tile myTile6;
+        private Tile myTile7;
+        private Tile myTile8;
         Board() {
-            setState(new Empty());
+            setState(new Empty(), myTile0);
+            setState(new Empty(), myTile1);
+            setState(new Empty(), myTile2);
+            setState(new Empty(), myTile3);
+            setState(new Empty(), myTile4);
+            setState(new Empty(), myTile5);
+            setState(new Empty(), myTile6);
+            setState(new Empty(), myTile7);
+            setState(new Empty(), myTile8);
         }
 
-        void setState(final Tile newState) {
-            myTile = newState;
+        void setState(final Tile newState, Tile tile) {
+            tile = newState;
         }
     }
 
@@ -26,4 +42,6 @@ public class AI {
     public class Traitors implements Tile {
 
     }
+
+    public class TicTacToeAI
 }
