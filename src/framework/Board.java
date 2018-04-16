fb629a685a93f40638dfc422fffe8e26e7a3c365
Dennis <dennisdeweme@gmail.com>
2018-04-16 17:44:55 +0200

    Counting on

--- src/framework/Board.java
+++ src/framework/Board.java
@@ -1,12 +1,16 @@
 package framework;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
 
 import java.util.ArrayList;
 
@@ -17,6 +21,11 @@ public class Board {
     ArrayList<Image> imagesReversie = new ArrayList<Image>();
     int[] move = new int[3];
     boolean moveMade;
+    private int yourPoints = 0;
+    private int loserPoints = 0;
+    private VBox hb;
+    private Label label;
+    private Label notLabel;
     String[] games = {"TicTacToe", "Reversi"};
 
     public Board(){
@@ -66,11 +75,13 @@ public class Board {
 
     public void drawBoard(int[][] field, String game){
         for(int y = 0; y < field.length; y++){
-            for(int x = 0; x < field[1].length; x++) {                          // later nog even terug komen om te kijken of de field.length en de field[1].length op de juiste plaats staan
+            for(int x = 0; x < field[1].length; x++) {
+                // later nog even terug komen om te kijken of de field.length en de field[1].length op de juiste plaats staan
                 Rectangle rect = new Rectangle(x, y, 200, 200);
                 if(game.equals(games[1])) {
                     rect = new Rectangle(x, y, 80, 80);
                 }
+                counter(field);
                 Rectangle finalRect = rect;
                 rect.setOnMouseClicked((e) -> fieldClicked(finalRect, game));
                 rect.setFill(Color.WHITE);
@@ -78,16 +89,44 @@ public class Board {
                 if(field[x][y] != 0){
                     setImage(rect, field[x][y], game);
                 }
+
                 gridPane.add(rect, x, y);
             }
         }
     }
 
+    private void counter(int[][] field){
+        loserPoints = 0;
+        yourPoints = 0;
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[i][j] == 1){
+                    yourPoints = yourPoints + 1;
+                    System.out.println("zijn er ook lege");
+                }else if(field[i][j] == 2){
+                    loserPoints = loserPoints + 1;
+                }else{
 
+                }
+            }
+            label.setText("Your Points:\n" + yourPoints);
+            notLabel.setText("Their points: \n" + loserPoints);
+        }
+
+
+    }
     public void start(Stage primaryStage, int[][] field, String name, String game) throws Exception {
         moveMade = false;
+        BorderPane root = new BorderPane();
+        hb = new VBox();
         gridPane = new GridPane();
-        Scene scene = new Scene(gridPane);
+        label = new Label("Your Points:\n" + yourPoints);
+        notLabel = new Label("Their points: \n" + loserPoints);
+        counter(field);
+        hb.getChildren().addAll(label, notLabel);
+        root.setCenter(gridPane);
+        root.setRight(hb);
+        Scene scene = new Scene(root);
         drawBoard(field, game);
         primaryStage.setTitle(name);
         primaryStage.setScene(scene);
